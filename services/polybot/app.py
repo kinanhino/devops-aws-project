e1a9f1763bde470a86ef987f0d525cfe5f312dfb
kinanhino <kinan.hino@e.braude.ac.il>
2023-12-22 23:21:42 +0200

    finished telegram bot service full functionality

--- services/polybot/app.py
+++ services/polybot/app.py
@@ -1,13 +1,50 @@
 import flask
 from flask import request
 import os
-from bot import ObjectDetectionBot
+from bot import ObjectDetectionBot, Bot
+import boto3
+from botocore.exceptions import ClientError
+from loguru import logger
+import json
+
+
+def get_secret():
+
+    secret_name = "kinan-aws-key"
+    region_name = "eu-central-1"
+    # Create a Secrets Manager client
+    session = boto3.session.Session(region_name=region_name)
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+        secret = get_secret_value_response['SecretString']
+        secret_dict = json.loads(secret)
+
+        tele_token = secret_dict["TELEGRAM_TOKEN"]
+
+    except ClientError as e:
+        logger.error(e)
+        return False
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return False
+    except KeyError:
+        print("Key 'TELEGRAM_TOKEN' not found in the JSON data")
+        return False
+
+    return tele_token
+
 
 app = flask.Flask(__name__)
 
-
 # TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+TELEGRAM_TOKEN = get_secret()
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
@@ -45,6 +82,7 @@ def load_test():
 
 
 if __name__ == "__main__":
+
     bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
 
     app.run(host='0.0.0.0', port=8443)
--- services/polybot/bot.py
+++ services/polybot/bot.py
@@ -1,13 +1,17 @@
+import boto3
 import telebot
 from loguru import logger
 import os
 import time
 from telebot.types import InputFile
+from botocore.exceptions import ClientError
 
 
 class Bot:
 
     def __init__(self, token, telegram_chat_url):
+        region_name = os.getenv('REGION')
+        self.session = boto3.Session(region_name=region_name)
         # create a new instance of the TeleBot class.
         # all communication with Telegram servers are done using self.telegram_bot_client
         self.telegram_bot_client = telebot.TeleBot(token)
@@ -66,12 +70,41 @@ class Bot:
 
 
 class ObjectDetectionBot(Bot):
+
+    def send_message_to_sqs(self, msg_body):
+        sqs_client = self.session.client('sqs')
+        queue_url = 'https://sqs.eu-central-1.amazonaws.com/933060838752/kinan-queue'
+        try:
+            response = sqs_client.send_message(
+                QueueUrl=queue_url,
+                MessageBody=msg_body
+            )
+            logger.info(response)
+        except ClientError as e:
+            print(f"An error occurred: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+    def upload_to_s3(self, file_path, bucket_name, object_name=None):
+        if object_name is None:
+            object_name = os.path.basename(file_path)
+
+        s3_client = self.session.client('s3')
+        try:
+            s3_client.upload_file(file_path, bucket_name, object_name)
+        except ClientError as e:
+            logger.error(e)
+            return False
+        return True
+
     def handle_message(self, msg):
         logger.info(f'Incoming message: {msg}')
 
         if self.is_current_msg_photo(msg):
             photo_path = self.download_user_photo(msg)
-
+            bucket_name = os.environ['BUCKET_NAME']
             # TODO upload the photo to S3
+            self.upload_to_s3(photo_path, bucket_name, photo_path)
             # TODO send a job to the SQS queue
+            self.send_message_to_sqs(photo_path)
             # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
+            self.send_text(msg['chat']['id'], f'Your image is being processed. Please wait...')
